6da88f78c8102a2076c28e6fc0f6d929dc842b46
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2022-08-03 15:57:58 -0700

    Fix payload

--- .ci/alert.sh
+++ .ci/alert.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 set -e
-curl --show-error --fail -s -X POST -H 'Content-Type: application/json' -d "{\"text\":\"$PAYLOAD\"}" $WEBHOOK_URL
+payload="{\"text\":\"$TARGET is down! [Click here for the log]($RUN_URL).\"}"
+curl \
+  --show-error \
+  --fail \
+  -s \
+  -X POST \
+  -H 'Content-Type: application/json' \
+  -d $payload \
+  $WEBHOOK_URL
